99931db547b4e2a42be67af8854bcc4442aa842a
Alan Ott <alan.ott@ecole-hexagone.com>
2025-03-04 23:21:30 +0000

    Rendering a Quad & engine/render file optimizations

--- engine-from-scratch/build.bat
+++ engine-from-scratch/build.bat
@@ -1,4 +1,4 @@
-set files=src\glad.c src\main.c
+set files=src\glad.c src\main.c src\engine\global.c src\engine\render\render.c src\engine\render\render_init.c
 set libs=S:\CodeProjects\OpenGLGameEngine\lib\SDL2main.lib S:\CodeProjects\OpenGLGameEngine\lib\SDL2.lib S:\CodeProjects\OpenGLGameEngine\lib\freetype.lib
 
 CL /Zi /I S:\CodeProjects\OpenGLGameEngine\include %files% /link %libs% /OUT:LaunchGame.exe
